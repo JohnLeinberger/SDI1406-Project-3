07cf468685afac346b23b87496998a7dff5863d5
John Leinberger <jleinberger@fullsail.edu>
2014-06-14 23:10:51 -0400

    Added JSON data and tested output.

--- Leinberger_John_Project3.js
+++ Leinberger_John_Project3.js
@@ -1,1 +1,31 @@
-alert("JavaScript works!");
+//John Leinberger
+//SDI 1406
+//Project 3
+//Project using own flowchart and JSON data
+
+
+var foodInventory = {
+    "inventory": [
+        {
+            "food": "Tomato Soup",
+            "quantity": 2
+        },
+        {
+            "food": "Spam",
+            "quantity": 1
+        },
+        {   "food": "Baked Beans",
+            "quantity": 3
+        }
+    ]
+};
+
+var jsonHandler = function (json){
+    for(var i = 0; i < json.inventory.length; i++){
+        var inventory = json.inventory[i];
+        console.log("Food type: " + inventory.food + " Quantity: " + inventory.quantity);
+    }
+};
+
+
+jsonHandler(foodInventory);

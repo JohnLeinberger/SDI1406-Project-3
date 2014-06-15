dca72fe2814ca260cc40aba9048c4ec8da4acb05
John Leinberger <jleinberger@fullsail.edu>
2014-06-14 23:44:09 -0400

    Added boolean function keepWalking. No bugs.

--- Leinberger_John_Project3.js
+++ Leinberger_John_Project3.js
@@ -3,7 +3,7 @@
 //Project 3
 //Project using own flowchart and JSON data
 
-
+//global variables
 var foodInventory = {
     "inventory": [
         {
@@ -20,12 +20,44 @@ var foodInventory = {
     ]
 };
 
-var jsonHandler = function (json){
+var walkMore = true;
+var searchForFood = false;
+
+//functions
+function jsonHandler (json){
     for(var i = 0; i < json.inventory.length; i++){
         var inventory = json.inventory[i];
         console.log("Food type: " + inventory.food + " Quantity: " + inventory.quantity);
     }
 };
 
+function keepWalking(confirmWalk) {
+    
+    //local variables
+    var keepGo = confirmWalk;
+    var lookForFood = true;
+    var counter = 0;
+    var limitWalk = 10;
+    var distanceLeft = 0;
+    
+    while(keepGo === true){
+        counter++;
+        distanceLeft = limitWalk - counter;
+        if (distanceLeft < 0) {
+            console.log("You have walked your limit of " + counter + " miles today, you must rest now.");
+            break;
+        }
+        console.log("You have walked " + counter + " miles today, you can walk another " + distanceLeft + " miles before you rest.");
+        keepGo = confirm("Do you want to keep on walking?");
+    }
+    
+    lookForFood = confirm("Do you want to search the area for food?");
+    return lookForFood;
+};
 
+//main code
 jsonHandler(foodInventory);
+
+walkMore = confirm("Do you want to continue walking down the road?");
+
+searchForFood = keepWalking(walkMore);
